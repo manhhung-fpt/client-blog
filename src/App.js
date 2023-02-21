825ee83a749a6c4678a62e3c508cb5c7164c9319
MSI\ADMIN <hungtmse150230@fpt.edu.vn>
2023-02-21 22:39:10 +0700

    u

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/create" element={<CreatePost />} />
+          <Route path="/create" element={<CreatePost />}/>
           <Route path="/post/:id" element={<PostPage />} />
           <Route path="/edit/:id" element={<EditPost />} />
         </Route>

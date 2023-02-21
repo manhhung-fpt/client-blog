cf5671599372443d6f69ceb420309d8ff65a39b6
MSI\ADMIN <hungtmse150230@fpt.edu.vn>
2023-02-21 23:16:29 +0700

    u

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
-      //credentials: '',
+      credentials: 'include',
     });
     if (response.ok) {
       response.json().then(userInfo => {

aa25969f3611b345fed5b59fc9e9ef6f6b66f9a2
MSI\ADMIN <hungtmse150230@fpt.edu.vn>
2023-02-21 22:34:41 +0700

    init

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,20 @@
+import Post from "../Post";
+import {useEffect, useState} from "react";
+
+export default function IndexPage() {
+  const [posts,setPosts] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/post`).then(response => {
+      response.json().then(posts => {
+        setPosts(posts);
+      });
+    });
+  }, []);
+  return (
+    <>
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post} />
+      ))}
+    </>
+  );
+}
